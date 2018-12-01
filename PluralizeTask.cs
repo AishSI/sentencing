e23610a405a16383f0befc19855da125dced57eb
AishSI <aishsi@gmail.com>
2018-12-01 16:54:45 +0700

    сделал ветвление наиболее понятным для восприятия - без вложений циклов

--- PluralizeTask.cs
+++ PluralizeTask.cs
@@ -1,19 +1,15 @@
 ﻿namespace Pluralize
 {
-    public static class PluralizeTask
-    {
-        public static string PluralizeRubles(int count)
-        {
-            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-            int lastNumber = count % 10;
-            int penultimateNumber = (count / 10) % 10;
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			int lastNumber = count % 10;
+			int penultimateNumber = (count / 10) % 10;
 
-            if (penultimateNumber != 1)
-            {
-                if(lastNumber == 1) return "рубль";
-                if (lastNumber == 2 || lastNumber == 3 || lastNumber == 4) return "рубля";                
-            }
-            return "рублей";
-        }
-    }
+			if (penultimateNumber != 1 && lastNumber == 1) return "рубль";
+			if (penultimateNumber != 1 && (lastNumber == 2 || lastNumber == 3 || lastNumber == 4)) return "рубля";
+			return "рублей";
+		}
+	}
 }
